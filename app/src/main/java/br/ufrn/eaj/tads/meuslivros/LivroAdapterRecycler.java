5ec0ba84441db2a7378dc28c067e4c1304987940
fbio_ <fbio_henrique15@hotmail.com>
2017-10-15 22:10:11 -0300

    Atualizaçao

--- app/src/main/java/br/ufrn/eaj/tads/meuslivros/LivroAdapterRecycler.java
+++ app/src/main/java/br/ufrn/eaj/tads/meuslivros/LivroAdapterRecycler.java
@@ -1,14 +1,20 @@
 package br.ufrn.eaj.tads.meuslivros;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import model.Livro;
@@ -20,16 +26,24 @@ public class LivroAdapterRecycler extends RecyclerView.Adapter {
     List<Livro> livroList;
     BancoHelper bancoHelper;
 
+    private static final int PENDING_REMOVAL_TIMEOUT = 3000;
+    List<Livro> itemsPendingRemoval;
+
+    private Handler handler = new Handler();
+    HashMap<Livro, Runnable> pendingRunnables = new HashMap<>();
+
+
     public LivroAdapterRecycler(Context context, List<Livro> livroList) {
         this.context = context;
         this.livroList = livroList;
         this.bancoHelper = new BancoHelper(context);
+        this.itemsPendingRemoval = new ArrayList<>();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(context).inflate(R.layout.livro_card_inflater, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.livro_inflater_novo, parent, false);
         LivroHolder holder = new LivroHolder(view);
         return holder;
     }
@@ -40,10 +54,35 @@ public class LivroAdapterRecycler extends RecyclerView.Adapter {
         LivroHolder livroHolder = (LivroHolder) holder;
         final Livro livroEscolhido = livroList.get(position);
 
-        livroHolder.textViewTitulo.setText(livroEscolhido.getTitulo());
-        livroHolder.textViewAutor.setText(livroEscolhido.getAutor());
-        livroHolder.textViewAno.setText(livroEscolhido.getAno());
-        livroHolder.textViewNota.setText(""+livroEscolhido.getNota());
+        if (itemsPendingRemoval.contains(livroEscolhido)) {
+            //view do swipe/delete
+            livroHolder.layoutNormal.setVisibility(View.GONE);
+            livroHolder.layoutGone.setVisibility(View.VISIBLE);
+            livroHolder.undoButton.setVisibility(View.VISIBLE);
+            livroHolder.undoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // usou o undo, remover a pendingRennable
+                    Runnable pendingRemovalRunnable = pendingRunnables.get(livroEscolhido);
+                    pendingRunnables.remove(livroEscolhido);
+                    if (pendingRemovalRunnable != null){
+                        handler.removeCallbacks(pendingRemovalRunnable);
+                    }
+                    itemsPendingRemoval.remove(livroEscolhido);
+                    //binda novamente para redesenhar
+                    notifyItemChanged(livroList.indexOf(livroEscolhido));
+                }
+            });
+        }else{
+            livroHolder.textViewTitulo.setText(livroEscolhido.getTitulo());
+            livroHolder.textViewAutor.setText(livroEscolhido.getAutor());
+            livroHolder.textViewAno.setText(livroEscolhido.getAno());
+            livroHolder.textViewNota.setText(""+livroEscolhido.getNota());
+            livroHolder.layoutNormal.setVisibility(View.VISIBLE);
+            livroHolder.layoutGone.setVisibility(View.GONE);
+            livroHolder.undoButton.setVisibility(View.GONE);
+            livroHolder.undoButton.setOnClickListener(null);
+        }
     }
 
     @Override
@@ -51,12 +90,64 @@ public class LivroAdapterRecycler extends RecyclerView.Adapter {
         return livroList == null ? 0 : livroList.size();
     }
 
+    public void removerComTempo(int position) {
+        final Livro livro = livroList.get(position);
+        if (!itemsPendingRemoval.contains(livro)) {
+            itemsPendingRemoval.add(livro);
+            notifyItemChanged(position);
+            Runnable pendingRemovalRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    remover(livroList.indexOf(livro));
+                }
+            };
+            handler.postDelayed(pendingRemovalRunnable, PENDING_REMOVAL_TIMEOUT);
+            pendingRunnables.put(livro, pendingRemovalRunnable);
+        }
+    }
+
+    public void remover(int position) {
+        Livro livro = livroList.get(position);
+        if (itemsPendingRemoval.contains(livro)) {
+            itemsPendingRemoval.remove(livro);
+            //Pode implementar como se fosse uma outra lista
+        }
+        if (livroList.contains(livro)) {
+            livroList.remove(position);
+            bancoHelper.delete(livro);
+            notifyItemRemoved(position);
+        }
+    }
+
+
+    public void mover(int fromPosition, int toPosition){
+
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(livroList, i, i + 1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(livroList, i, i - 1);
+            }
+        }
+
+
+        notifyItemMoved(fromPosition, toPosition);
+        notifyItemChanged(toPosition);
+        notifyItemChanged(fromPosition);
+    }
+
+
     public class LivroHolder extends RecyclerView.ViewHolder {
 
         final TextView textViewTitulo;
         final TextView textViewAutor;
         final TextView textViewAno;
         final TextView textViewNota;
+        final LinearLayout layoutNormal;
+        final LinearLayout layoutGone;
+        final Button undoButton;
 
         public LivroHolder(View v) {
             super(v);
@@ -64,7 +155,9 @@ public class LivroAdapterRecycler extends RecyclerView.Adapter {
             textViewAutor=  v.findViewById(R.id.autor);
             textViewAno=  v.findViewById(R.id.ano);
             textViewNota = v.findViewById(R.id.nota);
+            layoutNormal = v.findViewById(R.id.layout_normal);
+            layoutGone = v.findViewById(R.id.layout_gone);
+            undoButton = v.findViewById(R.id.undo_button);
         }
     }
-
 }
--- app/src/main/java/br/ufrn/eaj/tads/meuslivros/RecyclerActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/meuslivros/RecyclerActivity.java
@@ -3,12 +3,18 @@ package br.ufrn.eaj.tads.meuslivros;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 
@@ -42,61 +48,60 @@ public class RecyclerActivity extends AppCompatActivity {
 
         listaLivro = bancoHelper.findAll();
 
-        recyclerView.setAdapter(new SlideInLeftAnimationAdapter(new LivroAdapterRecycler(context, listaLivro)));
+        recyclerView.setAdapter(new LivroAdapterRecycler(context, listaLivro));
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
 
         recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.addOnItemTouchListener(new MyRecycler(RecyclerActivity.this, recyclerView, new MyRecycler.OnItemClickListener() {
-
+            //Verificar a virw que esta sendo clicada, pois no click de "desfazer" entra no edit(OnItemClick)
             @Override
             public void OnItemClick(View view, int i) {
 //              Toast.makeText(RecyclerActivity.this, "Clique simples", Toast.LENGTH_SHORT).show();
-                Intent it = new Intent(RecyclerActivity.this,CadastroActivity.class);
-                Bundle bundle = new Bundle();
-
-                bundle.putString("titulo",listaLivro.get(i).getTitulo());
-                bundle.putString("autor",listaLivro.get(i).getAutor());
-                bundle.putString("ano",listaLivro.get(i).getAno());
-                bundle.putFloat("nota", (float) listaLivro.get(i).getNota());
-                bundle.putLong("id",listaLivro.get(i).getId());
-
-                it.putExtras(bundle);
-
-                startActivity(it);
-
+//                Intent it = new Intent(RecyclerActivity.this,CadastroActivity.class);
+//                Bundle bundle = new Bundle();
+//
+//                bundle.putString("titulo",listaLivro.get(i).getTitulo());
+//                bundle.putString("autor",listaLivro.get(i).getAutor());
+//                bundle.putString("ano",listaLivro.get(i).getAno());
+//                bundle.putFloat("nota", (float) listaLivro.get(i).getNota());
+//                bundle.putLong("id",listaLivro.get(i).getId());
+//
+//                it.putExtras(bundle);
+//
+//                startActivity(it);
             }
 
             @Override
             public void OnItemLongClick(View view, final int position) {
                 //Toast.makeText(Main5Activity.this, "Clique longo", Toast.LENGTH_SHORT).show();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                builder.setTitle("Confimação")
-                        .setMessage("Tem certeza que deseja excluir este livro?")
-                        .setPositiveButton("Excluir", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                boolean sucesso = false;
-                                if(sucesso == false) {
-                                    final Livro removido = listaLivro.get(position);
-                                    listaLivro.remove(position);
-                                    bancoHelper.delete(removido);
-
-                                    recyclerView.getAdapter().notifyItemRemoved(position);
-                                    Snackbar.make((View)recyclerView.getParent(), "Excluiu!", Snackbar.LENGTH_SHORT)
-                                            .setAction("Action", null).show();
-                                }else{
-                                    Snackbar.make((View)recyclerView.getParent(), "Cancelou a operação!", Snackbar.LENGTH_SHORT)
-                                            .setAction("Action", null).show();
-                                }
-
-                            }
-                        })
-                        .setNegativeButton("Cancelar", null)
-                        .create()
-                        .show();
+//                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+//                builder.setTitle("Confimação")
+//                        .setMessage("Tem certeza que deseja excluir este livro?")
+//                        .setPositiveButton("Excluir", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                boolean sucesso = false;
+//                                if(sucesso == false) {
+//                                    final Livro removido = listaLivro.get(position);
+//                                    listaLivro.remove(position);
+//                                    bancoHelper.delete(removido);
+//
+//                                    recyclerView.getAdapter().notifyItemRemoved(position);
+//                                    Snackbar.make((View)recyclerView.getParent(), "Excluiu!", Snackbar.LENGTH_SHORT)
+//                                            .setAction("Action", null).show();
+//                                }else{
+//                                    Snackbar.make((View)recyclerView.getParent(), "Cancelou a operação!", Snackbar.LENGTH_SHORT)
+//                                            .setAction("Action", null).show();
+//                                }
+//
+//                            }
+//                        })
+//                        .setNegativeButton("Cancelar", null)
+//                        .create()
+//                        .show();
 
 //                final Livro removido = listaLivro.get(position);
 //                listaLivro.remove(position);
@@ -115,7 +120,66 @@ public class RecyclerActivity extends AppCompatActivity {
             }
         }));
 
-        recyclerView.setItemAnimator(new LandingAnimator());
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        ItemTouchHelper itemHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.START | ItemTouchHelper.END) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                //é usado para operações drag and drop
+                int fromPosition = viewHolder.getAdapterPosition();
+                int toPosition = target.getAdapterPosition();
+
+                LivroAdapterRecycler adapter = (LivroAdapterRecycler) recyclerView.getAdapter();
+
+                adapter.mover(fromPosition, toPosition);
+                return true;// true se moveu, se não moveu, retorne falso
+            }
+
+            @Override
+            public boolean isLongPressDragEnabled() {
+                //return false; se quiser, é possivel desabilitar o drag and drop
+                return true;
+            }
+
+            @Override
+            public boolean isItemViewSwipeEnabled() {
+                //return false; se quiser, é possivel desabilitar o swipe
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int posicao = viewHolder.getAdapterPosition();
+                LivroAdapterRecycler adapterRecycler = (LivroAdapterRecycler) recyclerView.getAdapter();
+                adapterRecycler.removerComTempo(posicao);
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                View itemView = viewHolder.itemView;
+                Drawable background = new ColorDrawable(Color.RED);
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+
+        });
+
+        itemHelper.attachToRecyclerView(recyclerView);
+
     }
 
     @Override
